657721332cf158130dfc4384ca2f1a59a2dff817
jihaad <jihaadied@gmail.com>
2026-03-30 23:48:27 +0300

    Add files via upload
    1. Gün çalışmaları eklendi

---
+++ Gun-01.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // import input and output methods
+using namespace std;
+int main() {
+    
+    cout << "Hello, welcome to c++!" << endl; // pass the output
+    
+    return 0; // Program ended
+
+
+    // Escape Sequence : 
+    //  (\n)  : new line 
+    //  (\t)  : tab 
+    //   (\")   to  make name between ""
+    //.   // single comment
+    //     /*     multiple comment */  
